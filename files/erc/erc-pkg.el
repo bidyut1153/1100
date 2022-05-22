131df6a90f7525a86e350f54a48bb7c33d9e3381
bidyut1153 <99074262+bidyut1153@users.noreply.github.com>
2022-05-22 17:47:30 +0530

    Add ERC files

---
+++ files/erc/erc-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from erc.el  -*- no-byte-compile: t -*-
+(define-package "erc" "5.4.1" "An Emacs Internet Relay Chat client" '((emacs "27.1")) :authors '(("Alexander L. Belikoff" . "alexander@belikoff.net")) :maintainer '("Amin Bandali" . "bandali@gnu.org") :keywords '("irc" "chat" "client" "internet") :url "https://www.gnu.org/software/emacs/erc.html")
